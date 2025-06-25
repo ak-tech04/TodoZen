18984a302e0b28bf3b3e24a4cbe383a344cbf2c5
aditya <ak.dev2004@gmail.com>
2025-06-25 16:30:55 +0530

    created todo list basic app

---
+++ script.js
@@ -0,0 +1,68 @@
+let form = document.querySelector("form");
+let userTask = document.querySelector(".user__input");
+let taskList = document.querySelector(".todo__list");
+let addTask = document.querySelector(".add-btn");
+
+addTask.addEventListener("click", (event) => {
+  event.preventDefault();
+  userInput = userTask.value;
+  createTask(userInput);
+});
+
+taskList.addEventListener("click", (event) => {
+  const classList = Array.from(event.target.classList);
+  if (classList.includes("edit-btn")) {
+    // console.log("edit btn clicked");
+    const btn = event.target;
+    editBtn(btn);
+  } else if (classList.includes("remove-btn")) {
+    // console.log("remove btn clicked");
+    const btn = event.target;
+    removeBtn(btn);
+  }
+});
+
+function editBtn(btn) {
+  // selecting todo task paragraph
+  const text = btn.parentNode.previousElementSibling.innerHTML;
+  userTask.value = `${text}`;
+  taskList.removeChild(btn.parentNode.parentNode);
+  userTask.focus();
+}
+function removeBtn(btn) {
+  // console.log(btn);
+  taskList.removeChild(btn.parentNode.parentNode);
+}
+function createTask(userInput) {
+  // Creating div--- task > taskPara & btnContainer > taskEditBtn & taskRemoveBtn
+  let task = document.createElement("div");
+  let taskPara = document.createElement("p");
+
+  let btnContainer = document.createElement("div");
+  let taskEditBtn = document.createElement("button");
+  let taskRemoveBtn = document.createElement("button");
+
+  // taskPara = text, editbtn = edit, removebtn = remove
+  taskPara.innerText = `${userInput}`;
+  taskEditBtn.innerText = "Edit";
+  taskRemoveBtn.innerText = "Remove";
+
+  btnContainer.classList.add("buttons");
+  taskEditBtn.classList.add("btn", "edit-btn");
+  taskRemoveBtn.classList.add("btn", "remove-btn");
+  task.classList.add("todo__items");
+  taskPara.classList.add("todo__task");
+
+  //   appending in task div
+  task.appendChild(taskPara);
+  task.appendChild(btnContainer);
+
+  //   appending in btnContainer div
+  btnContainer.appendChild(taskEditBtn);
+  btnContainer.appendChild(taskRemoveBtn);
+
+  //   adding in tasklist global div
+  taskList.appendChild(task);
+  //   clearing input field
+  userTask.value = "";
+}
